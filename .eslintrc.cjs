7436d92490d9eafabf370af89c4f94420d4f0655
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-07-30 14:26:11 +0500

    Добавил eslint в проект

---
+++ .eslintrc.cjs
@@ -0,0 +1,81 @@
+module.exports = {
+    root:    true,
+    extends: 'airbnb-base',
+    parser:  '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    env:     {
+        browser: true,
+    },
+    rules: {
+        'max-len':    ['error', 120, 4, { ignoreComments: true }],
+        'func-names': ['error', 'never'],
+        indent:       [
+            'error', 4, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] },
+        ],
+        'import/no-unresolved': [
+            2,
+            { 'caseSensitive': false }
+         ],
+        'key-spacing':     ['error', { align: 'value' }],
+        'no-multi-spaces': [
+            'error',
+            {
+                exceptions: {
+                    VariableDeclarator: true,
+                    ImportDeclaration:  true,
+                },
+            },
+        ],
+        'no-new':               'off',
+        'no-param-reassign':    'off',
+        'linebreak-style':      'off',
+        'no-underscore-dangle': [
+            'error',
+            { enforceInMethodNames: false, allowAfterThis: true },
+        ],
+        'padding-line-between-statements': [
+            'error',
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'return',
+            },
+            {
+                blankLine: 'always',
+                prev:      ['const', 'let', 'var'],
+                next:      '*',
+            },
+            {
+                blankLine: 'any',
+                prev:      ['const', 'let', 'var'],
+                next:      ['const', 'let', 'var'],
+            },
+            {
+                blankLine: 'always',
+                prev:      'if',
+                next:      '*',
+            },
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'if',
+            },
+            {
+                blankLine: 'always',
+                prev:      'import',
+                next:      '*',
+            },
+            {
+                blankLine: 'any',
+                prev:      'import',
+                next:      'import',
+            },
+            {
+                blankLine: 'always',
+                prev:      '*',
+                next:      'export',
+            },
+        ],
+        'arrow-parens': ['error', 'as-needed', { requireForBlockBody: true }],
+    },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,33 @@
-import "./styles/main.scss";
-import "./style.scss";
+import './styles/main.scss';
+import './style.scss';
+
+class Test {
+    _test = 1;
+
+    state: {
+        a: number;
+        abs: number;
+    };
+
+    a: number;
+
+    constructor() {
+        this._test = 2;
+        this.state = {
+            a:   1,
+            abs: 2,
+        };
+
+        this.a = 0;
+
+        this.test();
+    }
+
+    test():number {
+        this.a = 2;
+
+        return this.a;
+    }
+}
+
+export default Test;
