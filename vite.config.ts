9b7ad05b8ad6b1ac20ad60ddae5a3681bbf019c4
Iskandarov Timur <timur.iskandarov96@gmail.com>
2024-09-07 23:32:56 +0500

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
     root:  'src',
     base:  './',
     build: {
         rollupOptions: {
-            input: {
-                index: resolve(__dirname, './src/index.html'),
-            },
+            input: './src/index.html',
         },
         outDir:      '../dist',
         emptyOutDir: true,
